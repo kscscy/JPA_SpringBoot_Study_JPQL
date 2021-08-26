5e1e4c0f69ff2df14cdd5752be3360d20e0279ac
Charles Kang <kscscy@gmail.com>
2021-08-26 09:40:49 +0900

    fetch join & 일반 join

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -16,55 +17,72 @@ public class JpaMain {
         try {
             // 영속
 
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
             Member member1 = new Member();
-            member1.setUsername("관리자1");
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-/*
-//            String query = "select concat('a', 'b') From Member m";
-            String query = "select substring(m.username, 2, 3) From Member m";
-            List<String> result = em.createQuery(query, String.class).getResultList();
-            for (String s : result) {
-                System.out.println("s = " + s);
-            }
-
- */
-
             /*
-            String query = "select locate('de', 'abcdef') From Member m";
-            List<Integer> result = em.createQuery(query, Integer.class).getResultList();
-            for (Integer s : result) {
-                System.out.println("s = " + s);
+            String query = "select m From Member m join fetch m.team";
+
+            List<Member> result = em.createQuery(query, Member.class).getResultList();
+            for (Member member : result) {
+                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
             }
              */
 
             /*
-            String query = "select size(t.members) From Team t";
-            List<Integer> result = em.createQuery(query, Integer.class).getResultList();
-            for (Integer s : result) {
-                System.out.println("s = " + s);
+            String query = "select t From Team t join fetch t.members";
+
+            List<Team> result = em.createQuery(query, Team.class).getResultList();
+
+            System.out.println("result.size() = " + result.size());
+            
+            for (Team team : result) {
+                System.out.println("team.getName() = " + team.getName() + " || members=" + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println("member = " + member);
+                }
             }
              */
 
+            String query = "select  t From Team t  join fetch t.members";
 
-//            String query = "select function('group_concat', m.username) From Member m";
-            String query = "select group_concat(m.username) From Member m";
+            List<Team> result = em.createQuery(query, Team.class).getResultList();
 
-            List<String> result = em.createQuery(query, String.class).getResultList();
-            for (String s : result) {
-                System.out.println("s = " + s);
+            System.out.println("result.size() = " + result.size());
+
+            for (Team team : result) {
+                System.out.println("team.getName() = " + team.getName() + " || members=" + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println("member = " + member);
+                }
             }
+
 
 
             System.out.println("=============================");
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
